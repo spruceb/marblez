65205ae78544cf994f983796472165b07d931844
Spruce Bondera <sprucebondera@gmail.com>
2025-03-30 21:13:53 -0700

    Remove JavaScript files after TypeScript migration

--- src/engine/ecs/index.js
+++
@@ -1,4 +0,0 @@
-export { Entity } from './Entity.js';
-export { Component } from './Component.js';
-export { System } from './System.js';
-export { World } from './World.js';
--- src/factories/index.js
+++
@@ -1,3 +0,0 @@
-export { MarbleFactory } from './MarbleFactory.js';
-export { PlatformFactory } from './PlatformFactory.js';
-export { ObstacleFactory } from './ObstacleFactory.js';
